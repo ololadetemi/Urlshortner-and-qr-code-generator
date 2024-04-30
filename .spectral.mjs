81a5e6714b91e89e95cec2f23a579a7e285f2e19
ololadetoluwalase@gmail.com <ololadetoluwalase@gmail.com>
2024-04-30 23:09:49 +0100

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NDg2MQ/spectral.js?branch_slug=master&token=65eb0469-c8ff-42f4-82a0-25b39cd8a9c2";
+export default { extends: ruleset };
